88da77f593ae6fdff4c01caf199a46cbcf0cc055
Pham-Son2k3 <kken02245@gmail.com>
2025-03-22 18:03:54 +0700

    timer.h

--- incl/timer.h
+++ incl/timer.h
@@ -2,9 +2,9 @@
 #define TIMER_H
 
 #include <stm32f10x.h>
-#include <stm32f10x_tim.h> // Thêm d? dùng TIM
+#include <stm32f10x_tim.h> 
 
 void timer2_Init(void);
-void Delay_1ms(unsigned long time_ms); // Ð?ng b? v?i .c
+void Delay_1ms(unsigned long time_ms); 
 
 #endif
